89a15e4e67ea63aba4ed4211b0fe1b40029ac0e1
advaithca <advaith29042002@gmail.com>
2023-07-02 10:10:26 +0530

    Some initial files
    Just the standard tutorial file from the NVIDIA cuda article. Performs addition of a million numbers.

---
+++ programs/add.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<math.h>
+
+using namespace std;
+
+// function to add elements of two arrays
+void add(int n, float *x, float *y)
+{
+    for(int i=0; i<n; i++)
+    {
+        y[i] = x[i] + y[i];
+    }
+}
+
+int main()
+{
+    int N = 1<<20;
+
+    float *x = new float[N];
+    float *y = new float[N];
+
+    for(int i = 0; i < N; i++)
+    {
+        x[i] = 1.0f;
+        y[i] = 2.0f;
+    }
+
+    add(N, x, y);
+
+    float maxError = 0.0f;
+    for(int i = 0; i < N; i++)
+    {
+        maxError = fmax(maxError, fabs(y[i]-3.0f));
+    }
+
+    cout << "Max error: " << maxError << endl;
+
+    delete [] x;
+    delete [] y;
+
+    return 0;
+}
